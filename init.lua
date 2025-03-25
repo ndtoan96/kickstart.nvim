162a9c700996f7eb23c744342bd7cbd9d9e1f338
Nguyen Duc Toan <ntoan96@gmail.com>
2025-03-26 00:00:02 +0700

    add taplo

--- init.lua
+++ init.lua
@@ -681,6 +681,8 @@ require('lazy').setup({
         -- pyright = {},
         rust_analyzer = {},
         cmake = {},
+        tinymist = {},
+        taplo = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
